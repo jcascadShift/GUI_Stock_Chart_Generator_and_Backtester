924bde11027fe9be5413eeaae610721211f9dc9d
jcascadShift <joshuamclarenprog24@gmail.com>
2026-06-24 10:28:31 -0600

    refine git ignore

--- loadandlabelcsvdata.py
+++ loadandlabelcsvdata.py
@@ -1,8 +1,15 @@
+import sys
 import pandas as pd
 from pathlib import Path
 
+def resource_path(relative_path):
+    if getattr(sys, "frozen", False):
+        return Path(sys._MEIPASS) / relative_path
+
+    return Path.cwd() / relative_path
+
 def loadcsv():
-    file_path = Path.cwd() / 'data' / 'EURUSD1.csv'
+    file_path =  resource_path(Path('data') / 'EURUSD1.csv')
     if not file_path.exists():
         raise FileNotFoundError(f"file and folder not found: {file_path}\n")
 
